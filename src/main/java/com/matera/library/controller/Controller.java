f79ce4abb2a17fcdb28f92674c0745b00aaa1512
Renato Souto Maior <renato.souto@matera.com>
2016-10-19 14:47:52 -0200

    Update Project

---
+++ src/main/java/com/matera/library/controller/Controller.java
@@ -0,0 +1,5 @@
+package com.matera.library.controller;
+
+public class Controller {
+
+}
---
+++ src/main/java/com/matera/library/model/Livro.java
@@ -0,0 +1,83 @@
+package com.matera.library.model;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Livro {
+
+	@Id
+	private String id;
+
+	private String descricao;
+
+	private BigDecimal preco;
+
+	private Date data;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Livro [id=" + id + ", descricao=" + descricao + "]";
+	}
+
+}
